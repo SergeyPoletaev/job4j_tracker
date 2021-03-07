bdc5df5efb2f5067dd0f543b5a493f8bd091bb9b
SergeyPoletaev <svpoletaev@gmail.com>
2021-03-07 15:27:52 +0300

    6. Date. Отображение даты. [#388530]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,22 +1,29 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
-    private int id;
-    private String name;
+  private int id;
+  private String name;
+  private LocalDateTime created = LocalDateTime.now();
 
-    public int getId() {
-        return id;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public LocalDateTime getCreated() {
+    return created;
+  }
 }
