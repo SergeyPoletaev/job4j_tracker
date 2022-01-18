80f29351a0e0c7799dab35b0a0e42193f88210d9
SergeyPoletaev <info.elekit@yandex.ru>
2022-01-18 15:51:36 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/pojo/College.java
@@ -0,0 +1,20 @@
+package pojo;
+
+import java.time.LocalDate;
+
+public class College {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFirstName("Ivan");
+        student.setLastName("Ivanovich");
+        student.setSurname("Ivanov");
+        student.setGroup("15-P");
+        student.setStartDate(LocalDate.of(2022, 11, 1));
+        System.out.println("firstName: " + student.getFirstName());
+        System.out.println("lastName: " + student.getLastName());
+        System.out.println("surname: " + student.getSurname());
+        System.out.println("group: " + student.getGroup());
+        System.out.println("startDate: " + student.getStartDate());
+    }
+}
