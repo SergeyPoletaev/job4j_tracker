d5d760b14916ee104dbb699a8066c42b5e1043fe
SergeyPoletaev <info.elekit@yandex.ru>
2022-01-28 17:12:40 +0300

    2. Преобразования List в Map. [#10093 #257013]. Добавлен тест.

--- src/test/java/ru/job4j/tracker/collection/OrderConvertTest.java
+++ src/test/java/ru/job4j/tracker/collection/OrderConvertTest.java
@@ -9,11 +9,26 @@ import java.util.Map;
 public class OrderConvertTest {
 
     @Test
-    public void process() {
+    public void whenSingleElementList() {
         OrderConvert orderConvert = new OrderConvert();
         Order order = new Order("123", "test1");
         List<Order> orders = List.of(order);
         Map<String, Order> exp = Map.of(order.getNumber(), order);
+        Map<String, Order> rsl = orderConvert.process(orders);
+        Assert.assertEquals(exp, rsl);
+    }
+
+    @Test
+    public void whenWithDuplicateElementsList() {
+        OrderConvert orderConvert = new OrderConvert();
+        Order order1 = new Order("123", "test1");
+        Order order2 = new Order("124", "test2");
+        Order order3 = new Order("123", "test3");
+        List<Order> orders = List.of(order1, order2);
+        Map<String, Order> exp = Map.of(
+                order1.getNumber(), order3,
+                order2.getNumber(), order2
+        );
         Map<String, Order> rsl = orderConvert.process(orders);
         Assert.assertEquals(exp, rsl);
     }
