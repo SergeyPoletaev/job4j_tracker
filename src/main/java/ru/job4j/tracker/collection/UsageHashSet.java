8ae814bc8fd0f9433bc0b3b578ff88b392734afc
SergeyPoletaev <info.elekit@yandex.ru>
2022-01-28 09:55:02 +0300

    0. Set, HashSet, Iterator. [#10095 #256896]. Мелкие исправления.

--- src/main/java/ru/job4j/tracker/collection/UsageHashSet.java
+++ src/main/java/ru/job4j/tracker/collection/UsageHashSet.java
@@ -1,13 +1,16 @@
 package ru.job4j.tracker.collection;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class UsageHashSet {
 
     public static void main(String[] args) {
-        HashSet<String> autos = new HashSet<>();
+        Set<String> autos = new HashSet<>();
+        autos.add("Lada");
         autos.add("Lada");
         autos.add("BMW");
+        autos.add("Volvo");
         autos.add("Volvo");
         autos.add("Toyota");
         for (String auto : autos) {
