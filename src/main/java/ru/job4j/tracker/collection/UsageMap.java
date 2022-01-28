511805ee7f2e394fd15ed660266e9336a711650a
SergeyPoletaev <info.elekit@yandex.ru>
2022-01-28 17:16:32 +0300

    0. Map, HashMap [#212652 #256991]. Добавлены дубликаты.

--- src/main/java/ru/job4j/tracker/collection/UsageMap.java
+++ src/main/java/ru/job4j/tracker/collection/UsageMap.java
@@ -8,8 +8,10 @@ public class UsageMap {
     public static void main(String[] args) {
         Map<String, String> map = new HashMap<>();
         map.put("a@mai.ru", "Ivanov Ivan Ivanovich");
+        map.put("a@mai.ru", "Pavlov Pavel Pavlovich");
         map.put("b@mail.ru", "Petrov Petr Petrovich");
         map.put("c@mail.ru", "Sidorova Anna Sergeevna");
+        map.put("c@mail.ru", "Sidorova Anna Ivanovna");
         for (String key : map.keySet()) {
             System.out.println(key + " = " + map.get(key));
         }
