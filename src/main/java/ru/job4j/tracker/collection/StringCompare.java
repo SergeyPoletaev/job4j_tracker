50a219b7e8717bf2f018b4078e4aef2c93d43425
SergeyPoletaev <info.elekit@yandex.ru>
2022-01-30 10:53:55 +0300

    3. Компаратор для строк [#35008 #257692]. Оптимизация реализации метода.

--- src/main/java/ru/job4j/tracker/collection/StringCompare.java
+++ src/main/java/ru/job4j/tracker/collection/StringCompare.java
@@ -6,19 +6,12 @@ public class StringCompare implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        int rsl = 0;
         for (int i = 0; i < left.length() && i < right.length(); i++) {
-            rsl = Character.compare(left.charAt(i), right.charAt(i));
+            int rsl = Character.compare(left.charAt(i), right.charAt(i));
             if (rsl != 0) {
-                break;
+                return rsl;
             }
         }
-        if (rsl == 0 && left.length() < right.length()) {
-            rsl = -1;
-        }
-        if (rsl == 0 && left.length() > right.length()) {
-            rsl = 1;
-        }
-        return rsl;
+        return Integer.compare(left.length(), right.length());
     }
 }
