28c6185da76e78f74d37969c13b26938019ea691
SergeyPoletaev <info.elekit@yandex.ru>
2022-02-06 20:20:33 +0300

    2. Аттестация [#49777]

---
+++ src/main/java/ru/job4j/tracker/stream/Analyze.java
@@ -0,0 +1,93 @@
+package ru.job4j.tracker.stream;
+
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyze {
+
+    /**
+     * Метод averageScore вычисляет общий средний балл.
+     *
+     * @param stream поток учеников
+     * @return общий средний балл.
+     */
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream
+                .flatMap(p -> p.getSubjects().stream())
+                .mapToInt(Subject::getScore)
+                .average()
+                .orElse(0D);
+    }
+
+    /**
+     * Метод averageScoreBySubject вычисляет средний балл ученика по его предметам.
+     *
+     * @param stream поток учеников.
+     * @return список из объекта Tuple (имя ученика и средний балл).
+     */
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream
+                .map(p -> new Tuple(
+                        p.getName(),
+                        p.getSubjects().stream()
+                                .mapToInt(Subject::getScore)
+                                .average()
+                                .orElse(0D)))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Метод averageScoreByPupil вычисляет средний балл по всем предметам для каждого ученика.
+     *
+     * @param stream поток учеников.
+     * @return список из объектов Tuple (название предмета и средний балл).
+     */
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream
+                .flatMap(p -> p.getSubjects().stream())
+                .collect(Collectors.groupingBy(
+                        Subject::getName,
+                        LinkedHashMap::new,
+                        Collectors.averagingDouble(Subject::getScore)))
+                .entrySet().stream()
+                .map(e -> new Tuple(e.getKey(), e.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Метод bestStudent - возвращает лучшего ученика. Лучшим считается ученик с наибольшим баллом по всем предметам.
+     *
+     * @param stream поток учеников.
+     * @return объект Tuple (имя ученика, сумма баллов ученика по всем предметам)
+     */
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return stream
+                .map(p -> new Tuple(
+                        p.getName(),
+                        p.getSubjects().stream()
+                                .mapToInt(Subject::getScore)
+                                .sum()))
+                .max(Comparator.comparing(Tuple::getScore)).orElseGet(Tuple::new);
+    }
+
+    /**
+     * Метод bestSubject - возвращает предмет с наибольшим баллом для всех студентов.
+     *
+     * @param stream поток учеников.
+     * @return объект Tuple (имя предмета, сумма баллов каждого ученика по этому предмету)
+     */
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return stream
+                .flatMap(p -> p.getSubjects().stream())
+                .collect(Collectors.groupingBy(
+                        Subject::getName,
+                        LinkedHashMap::new,
+                        Collectors.summingDouble(Subject::getScore)))
+                .entrySet().stream()
+                .map(e -> new Tuple(e.getKey(), e.getValue()))
+                .max(Comparator.comparing(Tuple::getScore)).orElseGet(Tuple::new);
+    }
+}
